d12f16a90d007537a48a1189b798fe8d71f8f441
Coluzzi Andrea <coluzziandrea.dev@gmail.com>
2024-01-09 23:58:32 +0100

    feat: Adds hitboxes & hurtboxes for attack

---
+++ World/Grass.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+func create_grass_effect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
+	var main = get_tree().current_scene
+	main.add_child(grassEffect)
+	grassEffect.global_position = global_position
+
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
